f31d8801cb836b247fc2d03cca3243d65eab5081
Felipe Passos <felipekaires@hotmail.com>
2017-10-18 01:02:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -64,7 +64,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 
-function removeElementFromEndOfArray() {
+function removeElementFromEndOfArray(array) {
   array.slice(0,array.length - 1)
   return array
 }
